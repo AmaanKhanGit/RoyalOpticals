b5a1e9692bcd9a263e3b20a0d3eeae2b17503efe
AmaanKhanGit <amaan050606@gmail.com>
2025-11-26 16:08:05 +0530

    vite config FAQ for npm build

--- royal-opticals-main/vite.config.js
+++ royal-opticals-main/vite.config.js
@@ -18,7 +18,8 @@ export default defineConfig({
                 products: resolve(__dirname, 'products.html'),
                 services: resolve(__dirname, 'services.html'),
                 about: resolve(__dirname, 'about.html'),
-                contact: resolve(__dirname, 'contact.html'),
+                contact: resolve(__dirname, 'contact-us.html'),
+                faq: resolve(__dirname, 'FAQ.html'),
             }
         }
     }
