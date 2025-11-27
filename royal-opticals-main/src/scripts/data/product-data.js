fe616e0196e7251675e7583cf1ba0a88415557e0
AmaanKhanGit <amaan050606@gmail.com>
2025-11-27 16:59:14 +0530

    ready to collect product data

---
+++ royal-opticals-main/src/scripts/data/product-data.js
@@ -0,0 +1,19 @@
+const productData = [
+    {
+        id: 1,
+        img1: '',
+        img2: '',
+        img3: '',
+        title: '', //~ product title
+        price: '',//~ starting from
+        desc: '',
+        category: '', //~ Eyeglasses, Sunglasses, Mens , womens, kids , blue-light,
+        brand: '', //~ Ray-Ban, Kodak , Titan , Vouge , Fastrack`
+        size: [], //~ sizes available
+        color: [], //~ colors available
+        material: '', //~ frame material
+        shape: '', //~ frame shape
+        rating: '', //~ out of 5
+        reviews: '', //~ number of reviews
+    },
+]
